533d65a32aeb155eb9f302b1276170425d8b9492
liushalangying <liushalangying@gmail.com>
2014-05-24 00:58:44 +0800

    init project

---
+++ src/com/example/xijin/XijinActivity.java
@@ -0,0 +1,14 @@
+package com.example.xijin;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class XijinActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+}
